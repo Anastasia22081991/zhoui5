545690730b4a218e94e84093380e8b82d6a2f825
ho03 clouddnagmbhgmail.onmicrosoft.com <ho03@clouddnagmbhgmail.onmicrosoft.com>
2022-08-30 11:55:06 +0000

    button

--- webapp/controller/Customer.controller.js
+++ webapp/controller/Customer.controller.js
@@ -1,14 +1,23 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,MessageBox) {
         "use strict";
 
         return Controller.extend("at.clouddna.training03.zhoui5.controller.Customer", {
             onInit: function () {
+            },
+
+            onSavePressed: function(){
+                let oView = this.getView();
+                let oModel = oView.getModel();
+                let oData = oModel.getData();
+            
+                MessageBox.success(JSON.stringify(oData));
             },
 
             genderFormatter: function (sGender) {
